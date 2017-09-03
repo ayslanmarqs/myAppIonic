227ec8d52e89d7afcbed8b4cc3eca3761e7e5d4b
ayslanmarqs <ayslanmarqs@gmail.com>
2017-09-03 17:55:34 -0300

    Criada introdução e links.

--- src/pages/intro/intro.ts
+++ src/pages/intro/intro.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TabsPage } from "../tabs/tabs";
 
 /**
- * Generated class for the FeedPage page.
+ * Generated class for the IntroPage page.
  *
  * See http://ionicframework.com/docs/components/#navigation for more info
  * on Ionic pages and navigation.
@@ -10,22 +11,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-feed',
-  templateUrl: 'feed.html',
+  selector: 'page-intro',
+  templateUrl: 'intro.html',
 })
-export class FeedPage {
-
-  public nomeUsuario: string = 'Ayslan Marques';
+export class IntroPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  somaDoisNumeros(num1: number, num2: number): void {
-    console.log(num1 + num2);
-  }
-
   ionViewDidLoad() {
-    this.somaDoisNumeros(4, 5);
+    console.log('ionViewDidLoad IntroPage');
   }
 
+  goToTabsPage() {
+    this.navCtrl.push(TabsPage);
+  }
 }
