bd82d6d52df22d328061c9e530551b54239a790a
ayslanmarqs <ayslanmarqs@gmail.com>
2017-09-03 13:07:55 -0300

    function with parameters.

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -20,12 +20,12 @@ export class FeedPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  somaDoisNumeros(): void {
-    alert(5 + 5);
+  somaDoisNumeros(num1: number, num2: number): void {
+    alert(num1 + num2);
   }
 
   ionViewDidLoad() {
-    this.somaDoisNumeros();
+    this.somaDoisNumeros(4, 5);
   }
 
 }
